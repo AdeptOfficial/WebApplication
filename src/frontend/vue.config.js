bb7a96737415fe52175717407254f6929ade4808
Leland Moy <Leland.d.moy@gmail.com>
2022-07-17 17:05:28 -0500

    Combined Vue and Spring Boot
    Added coordination of VueJS and Spring Boot, and able to create a JAR file to host a "Hello World" program in one command.

--- src/frontend/vue.config.js
+++ src/frontend/vue.config.js
@@ -1,4 +1,18 @@
+// vue.config.js
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+
+  // don't collide with the Spring Boot application
+  devServer: {
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        ws: true,
+        changeOrigin: true
+      }
+    }
+  }
+
 })
